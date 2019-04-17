14b1ae21881a1fa75243b30f2fa97fc0e90af7d5
Diemo Schwarz <schwarz@ircam.fr>
2019-04-17 12:00:07 +0200

    keep bufferindex info for multi-buffer SOM

--- patches/lib/mubu-som-js/descriptor_som.js
+++ patches/lib/mubu-som-js/descriptor_som.js
@@ -1,10 +1,12 @@
 autowatch = 1;
-outlets = 4;
+outlets = 5;
 
 const math = require('math.min.js').math;
 math.config({randomSeed: 7});
 
 var descriptorData = [];
+var bufferdata = [];
+var bufferstart = [];
 var normalizedData = [];
 var dimensionCount;
 var mapSize = [7, 7];
@@ -47,6 +49,8 @@ function clear()
 {
   train.cancel();
   descriptorData = [];
+  bufferdata = [];
+  bufferstart = [];
   coordinates = [];
   distances = [];
   bestMatches = [];
@@ -66,6 +70,15 @@ function setMapSize()
     radiusStart = math.max(mapSize) / 5;
     radiusEnd = math.max(mapSize) / 30;
   }
+}
+
+function bufferindex()
+{
+    var buffer = arrayfromargs(arguments);
+
+    // keep list of start index in descriptorData for each buffer
+    bufferdata.push(buffer);
+    bufferstart.push(descriptorData.length);
 }
 
 function appendData()
@@ -204,7 +217,7 @@ function training()
   if (t < trainingLength)
   {
     trainingStep(t, trainingLength, rStep, alpha, winTimeStamp);
-    outlet(3, math.ceil(100 * (t / trainingLength)));
+    outlet(4, math.ceil(100 * (t / trainingLength)));
     t++;
   }
   else
@@ -353,8 +366,24 @@ function outputDataCoordinatesOnMap()
   vecSomCoordsY = vecSomCoords.map(function (vector) {
     return vector[1];
   });
-  outlet(1, vecSomCoordsX);
-  outlet(2, vecSomCoordsY);
+
+  // last buffer's end
+  bufferstart.push(descriptorData.length);
+
+  // got through buffers, cut lists by buffer limits
+  for (var i = 0; i < bufferdata.length; i++)
+  {
+      var bufferindex = bufferdata[i];
+      var start = bufferstart[i];
+      var end   = bufferstart[i + 1];
+      
+      // output current buffer
+      outlet(3, bufferindex);
+
+      // output sublist
+      outlet(2, vecSomCoordsY.slice(start, end));
+      outlet(1, vecSomCoordsX.slice(start, end));
+  }
 }
 
 function getData(index)
