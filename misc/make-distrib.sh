068bdb53a414cec5c6f4bd053edced6c9969b34d
Diemo Schwarz <schwarz@ircam.fr>
2023-10-25 18:32:42 +0200

    #105: make dist from clean clone of repo

--- misc/make-distrib.sh
+++ misc/make-distrib.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
-DISTDIR=/tmp/catart-mubu
-ZIPNAME=./catart-mubu-v$1.zip
-FILES=/tmp/camudist.txt
+tmp=~/Downloads
+distbase=catart-mubu
+distdir=$tmp/$distbase
+zipname=./$distbase-v$1.zip
+files=$tmp/camudist.txt
+
+# get current git master
+if [ -d $distdir ]; then
+    pushd $distdir
+    git pull
+    popd
+else
+    git clone https://github.com/Ircam-RnD/catart-mubu.git $distdir
+fi
 
 # get all dirs, write to list
 # 1st level only: git ls-tree $(git branch | grep \* | cut -d " " -f2) --name-only  >$FILES
 #find . -path ./.git -prune -o -name gen_dsp -prune -o -type d -print | sed 's|^\./?||'  >$FILES
 
 # get all tracked files
-git ls-tree -r master --name-only . | egrep -v '^(doc/|misc/|\.git)|/(maxtest|test)/|\.gendsp' >$FILES
+#git ls-tree -r master --name-only . | egrep -v '^(doc/|misc/|\.git)|/(maxtest|test)/|\.gendsp' >$FILES
+
+# filter out unwanted files
+pushd $tmp
+find $distdir | egrep -v '/(doc|misc|\.git)|/(maxtest|test)/|\.gendsp' | sed "s,$tmp/,," >$files
 
 # create bom file
 #mkbom -s -i $FILES $FILES.bom
 
+rm $zipname
+zip $zipname -@ <$files
+popd
 
-rm $ZIPNAME
-zip $ZIPNAME -@ <$FILES
 #ditto -c --bom $FILES.bom ../catart-mubu $ZIPNAME
-
