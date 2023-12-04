591e89a2d822705ca544bc6d9df45ef20716a939
Diemo Schwarz <schwarz@ircam.fr>
2023-12-04 18:50:34 +0100

    #105, #91: split out advanced examples zip

--- misc/make-distrib.sh
+++ misc/make-distrib.sh
@@ -7,9 +7,13 @@ fi
 
 tmp=~/Downloads
 distbase=catart-mubu
+xistbase=catart-mubu-advanced-examples
 distdir=$tmp/$distbase
+xistdir=$tmp/$xistbase
 zipname=./$distbase-v$1.zip
-files=$tmp/camudist.txt
+xipname=./$distbase-v$1-advanced-examples.zip
+files=$tmp/camudistfiles.txt
+xfiles=$tmp/camuadvancedfiles.txt
 
 # get current git master (pull or clone into temp $distdir)
 if [ -d $distdir ]; then
@@ -27,18 +31,26 @@ fi
 #old: get all tracked files
 #git ls-tree -r master --name-only . | egrep -v '^(doc/|misc/|\.git)|/(maxtest|test)/|\.gendsp' >$FILES
 
-# regexp to catch unwanted files/dirs
-unwanted='/(doc|misc|\.git)|/(maxtest|test)/|\.gendsp'
+
+# prepare for making zips that unpack to dist dirs (need to be one dir up)
+pushd $tmp
+rm -f $zipname $xipname
+
+# regexp to include dirs for advanced examples distrib (also used to exclude from main dist)
+xwanted='/(catoracle|spat)/'
+
+# regexp to catch unwanted files/dirs to exclude from main distrib (also exclude advanced examples)
+unwanted="/(doc|misc|\.git)|/(maxtest|test)/|\.gendsp|$xwanted"
 
 # get list of all files for zip, filter out unwanted files and dirs
-pushd $tmp
 find $distdir | egrep -v "$unwanted" | sed "s,$tmp/,," >$files
-
-# create bom file
-#mkbom -s -i $FILES $FILES.bom
-
-rm $zipname
 zip $zipname -@ <$files
-popd
 
-#ditto -c --bom $FILES.bom ../catart-mubu $ZIPNAME
+# rename repo such that advanced examples zip unpacks to $xistdir
+mv $distdir $xistdir
+find $xistdir | egrep    "$xwanted"  | sed "s,$tmp/,," >$xfiles
+zip $xipname -@ <$xfiles
+
+# move back so that next call doesn't need to clone 
+mv $xistdir $distdir
+popd
