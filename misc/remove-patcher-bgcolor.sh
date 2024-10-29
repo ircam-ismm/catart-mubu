2068c9283b72441b1202caf0d17bdc44d84bc6b0
Diemo Schwarz <schwarz@ircam.fr>
2024-10-29 17:00:24 +0100

    perl remover in HERE

--- misc/remove-patcher-bgcolor.sh
+++ misc/remove-patcher-bgcolor.sh
@@ -11,13 +11,13 @@ find "$dir" -name '*.max[ph][ae][tl]*' -print0 |
 
 	# remove locked_bgcolor and editing_bgcolor, and our special color patcher bgcolor
 	# (in place, but makes backup file .bak)
-	perl -p -i.bak -e '\
-if (m/^\s*"(locked|editing)_bgcolor" : \[/  ||  \
-    m/^\s*"bgcolor" : \[ 0.898, 0.898, 0.898, 1.0 \]/) \
-  $_ = "";'\
-	     "$f"
-
+	perl -p -i.bak - <<"EOF" "$f"
+if (m/^\s*"(locked|editing)_bgcolor" : \[/  ||
+    m/^\s*"bgcolor" : \[ 0.898, 0.898, 0.898, 1.0 \]/)
+{
+  $_ = '';
+}
+EOF
 	# fix trailing commas on whole json string
 	perl -0777 -p -i -e 's/,(\s*)}/\1}/g' "$f"
     done
-
